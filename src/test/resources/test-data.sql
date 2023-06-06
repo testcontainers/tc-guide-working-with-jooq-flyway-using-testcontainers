2260531a13b2cf1470f18219b7e353cf6c3eff4e
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-06-06 09:03:57 +0530

    Update guide content

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -2,8 +2,9 @@ DELETE FROM comments;
 DELETE FROM posts;
 DELETE FROM users;
 
-INSERT INTO users(id, name, email) VALUES (1, 'Siva', 'siva@gmail.com');
-INSERT INTO users(id, name, email) VALUES (2, 'Oleg', 'oleg@gmail.com');
+INSERT INTO users(id, name, email) VALUES
+(1, 'Siva', 'siva@gmail.com'),
+(2, 'Oleg', 'oleg@gmail.com');
 
 INSERT INTO posts(id, title, content, created_by, created_at) VALUES
 (1, 'Post 1 Title', 'Post 1 content', 1, CURRENT_TIMESTAMP),
@@ -12,5 +13,4 @@ INSERT INTO posts(id, title, content, created_by, created_at) VALUES
 INSERT INTO comments(id, name, content, post_id, created_at) VALUES
 (1, 'Ron', 'Comment 1', 1, CURRENT_TIMESTAMP),
 (2, 'James', 'Comment 2', 1, CURRENT_TIMESTAMP),
-(3, 'Robert', 'Comment 3', 2, CURRENT_TIMESTAMP)
-;
+(3, 'Robert', 'Comment 3', 2, CURRENT_TIMESTAMP);
